c51bc3048df121cf1941299b3132c8022bf96016
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-29 18:44:01 -0500

    UPDATE: creating a component

--- CornVideo/src/playlist/components/media.js
+++ CornVideo/src/playlist/components/media.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 class Media extends React.Component{
     render(){
-        
+        <div>
+            <div>
+                <img></img>
+                <h3>This is my first component</h3>
+                <p>By: David Ruiz</p>
+            </div>
+        </div>
     }
 }
+
+export default Media
