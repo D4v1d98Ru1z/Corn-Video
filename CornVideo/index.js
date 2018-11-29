4b506a09271ba95bf89ef98ee47e5fdd5a638165
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-28 19:20:06 -0500

    UPDATE: using reactDOm

--- CornVideo/index.js
+++ CornVideo/index.js
@@ -1,1 +1,7 @@
-console.log(`It's working`)
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const app = document.getElementById('app')
+const Template = <h1>Welcome to CornVideo</h1>
+
+ReactDOM.render(Template, app)
