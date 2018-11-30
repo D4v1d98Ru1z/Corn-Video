8e9493549c6df5ec7254c4ed8f7ee70297c370d1
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-30 15:08:26 -0500

    UPDATE: adding props

--- CornVideo/src/playlist/components/media.js
+++ CornVideo/src/playlist/components/media.js
@@ -6,13 +6,13 @@ class Media extends Component{
         return(
             <div className="Media">
                 <div>
-                    <img className="Media-image" src="./images/series/titans.jpg" width={260} height={160} />
-                    <h3 className="Media-title">Titans</h3>
-                    <p className="Media-year">2018</p>
+                    <img className="Media-image" src={this.props.image} width={260} height={160} />
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p className="Media-year">{this.props.year}</p>
                 </div>
             </div>
         )
-    }
+    }    
 }
 
 export default Media
