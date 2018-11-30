07f8efb0272f0c04e4746f93e33221e5b82a29c8
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-29 20:22:00 -0500

    UPDATE: adding styles to the component

--- CornVideo/src/playlist/components/media.js
+++ CornVideo/src/playlist/components/media.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './media.css'
 
 class Media extends Component{
     render(){
         return(
-            <div>
+            <div className="Media">
                 <div>
-                    <img></img>
+                    <img src="./images/covers/bitcoin.jpg" width={260} height={160} />
                     <h3>This is my first component</h3>
                     <p>By: David Ruiz</p>
                 </div>
