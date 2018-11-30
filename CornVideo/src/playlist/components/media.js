3d8c098e9aaa96f229de2676003eb9ee83fc02f8
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-30 15:37:27 -0500

    UPDATE: proptype validation, done

--- CornVideo/src/playlist/components/media.js
+++ CornVideo/src/playlist/components/media.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 import './media.css'
+import PropTypes from 'prop-types'
 
 class Media extends Component{
     render(){
+        const {title, year, image} = this.props
         return(
             <div className="Media">
                 <div>
-                    <img className="Media-image" src={this.props.image} width={260} height={160} />
-                    <h3 className="Media-title">{this.props.title}</h3>
-                    <p className="Media-year">{this.props.year}</p>
+                    <img className="Media-image" src={image} width={260} height={160} />
+                    <h3 className="Media-title">{title}</h3>
+                    <p className="Media-year">{year}</p>
                 </div>
             </div>
         )
     }    
+}
+
+Media.propTypes = {
+    title: PropTypes.string.isRequired,
+    year: PropTypes.number,
+    image: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio'])
 }
 
 export default Media
