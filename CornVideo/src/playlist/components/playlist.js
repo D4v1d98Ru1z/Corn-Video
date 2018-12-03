a1dfba82e4870b6211d17e6cf7dad092160adce1
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-02 20:47:09 -0500

    UPDATE: adding styles

--- CornVideo/src/playlist/components/playlist.js
+++ CornVideo/src/playlist/components/playlist.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Media from './media'
+import './playlist.css'
 
 class Playlist extends Component{
     render(){
         const PLAYLIST = this.props.data.categories[0].playlist
         console.log(this.props.data)
         return(
-            <div>
+            <div className="Playlist">
                 {
                     PLAYLIST.map((item)=>{
                         return <Media {...item} key={item.id} />
