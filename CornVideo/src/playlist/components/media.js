fa29832de8aa9a3a2d3772230397227a099e51b4
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-04 18:29:06 -0500

    UPDATE: setting pure components

--- CornVideo/src/playlist/components/media.js
+++ CornVideo/src/playlist/components/media.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import './media.css'
 import PropTypes from 'prop-types'
 
-class Media extends Component{
+class Media extends PureComponent{
     render(){
         const {title, year, poster} = this.props
         return(
