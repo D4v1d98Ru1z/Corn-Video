f258dfed0841f141e56f6ee505f43e51350b4ae3
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-04 17:40:26 -0500

    UPDATE: creating the svg

--- CornVideo/src/icons/components/icon.js
+++ CornVideo/src/icons/components/icon.js
@@ -1,10 +1,11 @@
 import React  from 'react'
 
-function Icon(){
+function Icon(props){
+    const {size, color} = props
     return(
-        <div>
-
-        </div>
+        <svg width={size} height={size} viewBox="0 0 32 32" fill={color}>
+            {props.children}
+        </svg>
     )
 }
 
