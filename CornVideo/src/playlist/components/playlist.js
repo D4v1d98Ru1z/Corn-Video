c1893225f863951f7170eb1a9a8d52779c36df65
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-04 18:02:08 -0500

    ADD: volume component

--- CornVideo/src/playlist/components/playlist.js
+++ CornVideo/src/playlist/components/playlist.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Media from './media'
 import './playlist.css'
 import Play from '../../icons/components/play'
+import Volume from '../../icons/components/volume'
 function Playlist(props){
         const PLAYLIST = props.data.categories[0].playlist
         return(
             <div className="Playlist">
-                <Play size={50} color="blue"/>
+                <Play size={20} color="#1F1C21"/>
+                <Volume size={20} color="#1F1C21" />
                 {
                     PLAYLIST.map((item)=>{
                         return <Media {...item} key={item.id} />
