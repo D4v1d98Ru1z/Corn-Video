edb0d47ba8191783565cf0304475f9b8d5a452b8
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-04 17:29:19 -0500

    UPDATE: importing Play in playlist component

--- CornVideo/src/playlist/components/playlist.js
+++ CornVideo/src/playlist/components/playlist.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Media from './media'
 import './playlist.css'
-
+import Play from '../../icons/components/play'
 function Playlist(props){
         const PLAYLIST = props.data.categories[0].playlist
         return(
             <div className="Playlist">
+                <Play/>
                 {
                     PLAYLIST.map((item)=>{
                         return <Media {...item} key={item.id} />
