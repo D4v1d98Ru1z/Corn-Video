6b72e6757a9dfd4e9de29b5597331a9eb856e088
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-04 18:10:06 -0500

    UPDATE: importing the pause component

--- CornVideo/src/playlist/components/playlist.js
+++ CornVideo/src/playlist/components/playlist.js
@@ -3,12 +3,15 @@ import Media from './media'
 import './playlist.css'
 import Play from '../../icons/components/play'
 import Volume from '../../icons/components/volume'
+import Pause from '../../icons/components/pause'
+
 function Playlist(props){
         const PLAYLIST = props.data.categories[0].playlist
         return(
             <div className="Playlist">
                 <Play size={20} color="#1F1C21"/>
                 <Volume size={20} color="#1F1C21" />
+                <Pause size={20} color="#1F1C21" />
                 {
                     PLAYLIST.map((item)=>{
                         return <Media {...item} key={item.id} />
