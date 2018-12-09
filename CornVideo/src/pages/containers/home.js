5ff36f968543ea0b1a095b385fa65dcec3028303
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-09 17:39:17 -0500

    UPDATE: resiving all the categories props

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import HomeLayout from '../components/home-layout'
+import Categories from '../../categories/components/categories'
 
 class Home extends Component{
     render(){
         return(
             <HomeLayout>
-                HI 5
+                <Categories categories={this.props.data.categories} />
             </HomeLayout>
         )
     }
