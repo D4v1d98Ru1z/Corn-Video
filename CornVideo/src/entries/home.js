6ec009b02b47bf976c7559f0376cbe9e2dc267ac
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-09 16:40:30 -0500

    UPDATE:  adding the home containter into home entry

--- CornVideo/src/entries/home.js
+++ CornVideo/src/entries/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Playlist from './src/playlist/components/playlist'
-import data from './src/api.json'
+import Home from '../pages/containers/home'
+//import data from './src/api.json'
 
 const app = document.getElementById('app')
-render(<Playlist data={data} />, app)
+render(<Home />, app)
