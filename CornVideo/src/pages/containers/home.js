5362fc0e30d2a6c77e18205c14a2e6741599d671
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-09 16:36:19 -0500

    UPDATE: importing home layout into container

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import HomeLayout from '../components/home-layout'
 
 class Home extends Component{
     render(){
         return(
-
+            <HomeLayout/>
         )
     }
 }
