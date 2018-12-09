2b6ffd3a48f8ee1719d99d5c6080960de55f8451
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-09 17:38:52 -0500

    UPDATE: importing the API

--- CornVideo/src/entries/home.js
+++ CornVideo/src/entries/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Home from '../pages/containers/home'
-//import data from './src/api.json'
+import data from '../api.json'
 
 const app = document.getElementById('app')
-render(<Home />, app)
+render(<Home data={data} />, app)
