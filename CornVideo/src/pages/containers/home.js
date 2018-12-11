f446afd0a03bd27ae3f1c0c2cd7877aeb42a314d
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-11 18:12:48 -0500

    UPDATE: importing modal component

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -3,6 +3,7 @@ import HomeLayout from '../components/home-layout'
 import Categories from '../../categories/components/categories'
 import Related from '../components/related'
 import ModalContainer from '../../widgets/containers/modal'
+import Modal from '../../widgets/components/modal'
 
 class Home extends Component{
     render(){
@@ -11,7 +12,9 @@ class Home extends Component{
                 <Related />
                 <Categories categories={this.props.data.categories} />
                 <ModalContainer>
-                    <h1>Hello from Modal</h1>
+                    <Modal>
+                        <h1>Hello from Modal</h1>
+                    </Modal>
                 </ModalContainer>
             </HomeLayout>
         )
