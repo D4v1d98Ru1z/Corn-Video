9470860c322dc69eb94602fc0806df407e9a0b49
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-11 18:12:27 -0500

    ADD: modal component

---
+++ CornVideo/src/widgets/components/modal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import './modal.css'
+
+function Modal(props){
+    return(
+        <div>
+            this is a modal
+            {props.children}
+        </div>
+    )
+}
+
+export default Modal
