8fe71a63d4b9f97705d148effa878926a68725b1
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-11 18:27:22 -0500

    UPDATE: set the handle modal event

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -6,13 +6,18 @@ import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal'
 
 class Home extends Component{
+    handleCloseModal = (event) =>{
+        this.setState({
+            modalVisible: false
+        })
+    }
     render(){
         return(
             <HomeLayout>
                 <Related />
                 <Categories categories={this.props.data.categories} />
                 <ModalContainer>
-                    <Modal>
+                    <Modal handleClick={this.handleCloseModal} >
                         <h1>Hello from Modal</h1>
                     </Modal>
                 </ModalContainer>
