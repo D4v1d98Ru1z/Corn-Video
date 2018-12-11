3f85bbd837eac9eefdf106626e45c16319a8210c
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-10 20:36:08 -0500

    UPDATE: creating the related component

---
+++ CornVideo/src/pages/components/related.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../../images/logo.png'
+
+
+function Related(props){
+    return(
+        <div>
+            <img src={Logo} width={250} />
+        </div>
+    )
+}
+
+export default Related
