bdde6f9f67169bc44aeb0bebdd0b568a6122e3a7
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-12 14:30:35 -0500

    UPDATE: importing the container component into home

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -4,6 +4,7 @@ import Categories from '../../categories/components/categories'
 import Related from '../components/related'
 import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal'
+import HandleError from '../../error/container/handle-error'
 
 class Home extends Component{
     state = {
@@ -23,21 +24,23 @@ class Home extends Component{
     }
     render(){
         return(
-            <HomeLayout>
-                <Related />
-                <Categories categories={this.props.data.categories} 
-                    handleOpenModal={this.handleOpenModal}
-                />
-                {
-                    this.state.modalVisible &&
-                        <ModalContainer>
-                            <Modal handleClick={this.handleCloseModal} >
-                                <h1>Hello from Modal</h1>
-                            </Modal>
-                        </ModalContainer>
+            <HandleError>
+                <HomeLayout>
+                    <Related />
+                    <Categories categories={this.props.data.categories} 
+                        handleOpenModal={this.handleOpenModal}
+                    />
+                    {
+                        this.state.modalVisible &&
+                            <ModalContainer>
+                                <Modal handleClick={this.handleCloseModal} >
+                                    <h1>Hello from Modal</h1>
+                                </Modal>
+                            </ModalContainer>
 
-                }
-            </HomeLayout>
+                    }
+                </HomeLayout>
+            </HandleError>
         )
     }
 }
