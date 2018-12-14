f7aaf47d57002ab39855c88309475768fb19f835
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-14 12:46:53 -0500

    UPDATE: importing the video player in home

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -5,6 +5,7 @@ import Related from '../components/related'
 import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal'
 import HandleError from '../../error/container/handle-error'
+import VideoPlayer from '../../player/containers/video-player'
 
 class Home extends Component{
     state = {
@@ -27,6 +28,7 @@ class Home extends Component{
             <HandleError>
                 <HomeLayout>
                     <Related />
+                    <VideoPlayer/>
                     <Categories categories={this.props.data.categories} 
                         handleOpenModal={this.handleOpenModal}
                     />
