f9d8341abdccbbd98093b45ffac2edf1d857a832
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-13 19:19:45 -0500

    ADD: search component

---
+++ CornVideo/src/widgets/components/search.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './search.css'
+
+const Search = () => (
+    <form className="Search" >
+        <input type="text" className="Search-input" />
+    </form>
+)
+
+export default Search
