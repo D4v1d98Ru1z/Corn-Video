be1c6a170a47d25a230772dd8192d4a2da175702
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-16 20:44:39 -0500

    UPDATE: importing the component

--- CornVideo/src/player/containers/video-player.js
+++ CornVideo/src/player/containers/video-player.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer extends Component{
+    state={
+        pause: true
+    }
+    togglePlay = (event) =>{
+        this.setState({
+            pause: !this.state.pause
+        })
+    }
     render(){
         return(
             <VideoPlayerLayout>
                 <Title
                 title="Hello from title"
                 />
+                <PlayPause 
+                pause={this.state.pause}
+                handleClick={this.togglePlay}
+                />
                 <Video 
-                autoplay={true}
+                autoplay={false}
                 src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
                 />
             </VideoPlayerLayout>
