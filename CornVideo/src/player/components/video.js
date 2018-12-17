729819a875d7dd04df11bff75c4e072c7b138499
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-16 21:28:59 -0500

    UPDATE: adding logic to play and pause

--- CornVideo/src/player/components/video.js
+++ CornVideo/src/player/components/video.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import './video.css'
 class Video extends Component{
+    togglePlay(){
+        if(this.props.pause){
+            this.video.play()
+        }
+        else{
+            this.video.pause()
+        }
+    }
+    componentWillReceiveProps(nextProps){
+        if(nextProps.pause !== this.props.pause){
+            this.togglePlay()
+        }
+    }
+    setRef = element =>{
+        this.video = element
+    }
     render(){
         return(
             <div className="Video">
                 <video
                 autoPlay={this.props.autoplay}
+                ref={this.setRef}
                 src={this.props.src}
                 />
             </div>
