052732ae094086e50c18565f9e6802204aa95b7e
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-20 13:38:05 -0500

    UPDATE: importing loading

--- CornVideo/src/player/containers/video-player.js
+++ CornVideo/src/player/containers/video-player.js
@@ -7,6 +7,7 @@ import Timer from '../components/timer'
 import Controls from '../components/video-player-controls'
 import {formattedTime} from '../../libs/utilities'
 import ProgressBar from '../components/progress-bar'
+import Spinner from '../components/spinner'
 
 class VideoPlayer extends Component{
     state={
@@ -15,6 +16,7 @@ class VideoPlayer extends Component{
         currentTime: 0,
         floatTime: 0,
         progress: 0,
+        loading: false
     }
     togglePlay = (event) =>{
         this.setState({
@@ -43,6 +45,16 @@ class VideoPlayer extends Component{
     handleProgressChange = event =>{
         this.video.currentTime = event.target.value
     }
+    handleSeeking = event => {
+        this.setState({
+            loading: true
+        })
+    }
+    handleSeeked = event => {
+        this.setState({
+            loading: false
+        })
+    }
 
     render(){
         return(
@@ -65,13 +77,17 @@ class VideoPlayer extends Component{
                     handleProgressChange={this.handleProgressChange}
                     />
                 </Controls>
-
+                <Spinner
+                active={this.state.loading}
+                />
                 <Video 
                 autoplay={this.props.autoplay}
                 pause={this.state.pause}
                 src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
                 handleLoadedMetadata={this.handleLoadedMetadata}
                 handleTimeUpdate={this.handleTimeUpdate}
+                handleSeeking={this.handleSeeking}
+                handleSeeked={this.handleSeeked}                
                 />
             </VideoPlayerLayout>
         )
