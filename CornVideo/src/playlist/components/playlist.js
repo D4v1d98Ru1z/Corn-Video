9602034ca5acf1403798c8a038608864d5a6095c
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-23 15:10:56 -0500

    UPDATE: open modal

--- CornVideo/src/playlist/components/playlist.js
+++ CornVideo/src/playlist/components/playlist.js
@@ -7,7 +7,7 @@ function Playlist(props){
             <div className="Playlist">
                 {
                     props.playlist.map((item)=>{
-                        return <Media {...item} key={item.id} handleClick={props.handleOpenModal} />
+                        return <Media {...item} key={item.id} openModal={props.handleOpenModal} />
                     })
                 }
             </div>
