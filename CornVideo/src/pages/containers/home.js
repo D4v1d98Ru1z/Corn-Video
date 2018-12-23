3a6543eeabeb93998fb3fd6c92722a127734a621
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-23 15:12:26 -0500

    UPDATE: getting the api values

--- CornVideo/src/pages/containers/home.js
+++ CornVideo/src/pages/containers/home.js
@@ -12,9 +12,10 @@ class Home extends Component{
         modalVisible: false
     }
 
-    handleOpenModal = (event) =>{
+    handleOpenModal = media =>{
         this.setState({
-            modalVisible: true
+            modalVisible: true,
+            media
         })
     }
 
@@ -28,15 +29,18 @@ class Home extends Component{
             <HandleError>
                 <HomeLayout>
                     <Related />
-                    <VideoPlayer/>
                     <Categories categories={this.props.data.categories} 
                         handleOpenModal={this.handleOpenModal}
-                    />
+                        />
                     {
                         this.state.modalVisible &&
-                            <ModalContainer>
+                        <ModalContainer>
                                 <Modal handleClick={this.handleCloseModal} >
-                                    <h1>Hello from Modal</h1>
+                                    <VideoPlayer
+                                        autoplay
+                                        src={this.state.media.src}
+                                        title={this.state.media.title}
+                                    />
                                 </Modal>
                             </ModalContainer>
 
