0bc26938ee1e260e6492192bf8ff52d525b21c98
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-12-25 14:25:59 -0500

    UPDATE: Showing the content from API

--- CornVideo/src/pages/components/related.js
+++ CornVideo/src/pages/components/related.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Logo from '../../../images/logo.png'
 import './related.css'
+import MyPlaylist from './my-playlist'
 
 function Related(props){
     return(
         <div className="Related" >
             <img src={Logo} width={250} />
+            <h5>My Playlist</h5>
+            {
+                props.myPlaylist.map(item => {
+                    return(
+                        <MyPlaylist 
+                            key={item.id} 
+                            {...item}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
