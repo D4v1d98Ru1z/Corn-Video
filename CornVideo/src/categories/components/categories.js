8467723152627b75121e823b3d42245d00017154
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-05 13:08:32 -0500

    UPDATE: importing the profile component

--- CornVideo/src/categories/components/categories.js
+++ CornVideo/src/categories/components/categories.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Category from './category'
 import './categories.css'
 import Search from '../../widgets/containers/search'
+import Profile from './../../widgets/components/profile'
 
 function Categories(props){
     return(
         <div className="Categories" >
             <Search/>
+            {
+                props.profile.map(item => {
+                    return(
+                        <Profile key={item.id} {...item}/>
+                    )
+                })
+            }
             {
                 props.categories.map((item) => {
                     return (
