7d283f8f4e8efe5566d4c7bbc1237878f54ce1aa
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-05 13:07:40 -0500

    ADD: new icon

---
+++ CornVideo/src/icons/components/arrow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Icon from './icon'
+
+function Arrow (props){
+    return(
+        <Icon {...props}>
+            <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"></path>            
+        </Icon>
+    )
+}
+
+export default Arrow
