ddc0bedf7df7df8684a61fd3d8634d3ba9826eec
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-01-07 15:34:42 -0500

    UPDATE: changing for  a Pure component

--- CornVideo/src/categories/components/categories.js
+++ CornVideo/src/categories/components/categories.js
@@ -1,35 +1,37 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Category from './category'
 import './categories.css'
 import Search from '../../widgets/containers/search'
 import Profile from './../../widgets/components/profile'
 
-function Categories(props){
-    return(
-        <div className="Categories" >
-            <div className="Container">
-                <div className="Search">
-                    <Search/>
+class Categories extends PureComponent{
+    render(){
+        return(
+            <div className="Categories" >
+                <div className="Container">
+                    <div className="Search">
+                        <Search/>
+                    </div>
+                    <div className="Prof">
+                        {
+                            this.props.profile.map(item => {
+                                return(
+                                    <Profile key={item.id} {...item}/>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-                <div className="Prof">
-                    {
-                        props.profile.map(item => {
-                            return(
-                                <Profile key={item.id} {...item}/>
-                            )
-                        })
-                    }
-                </div>
+                {
+                    this.props.categories.map((item) => {
+                        return (
+                            <Category key={item.id} {...item} handleOpenModal={this.props.handleOpenModal} />
+                        )
+                    })
+                }
             </div>
-            {
-                props.categories.map((item) => {
-                    return (
-                        <Category key={item.id} {...item} handleOpenModal={props.handleOpenModal} />
-                    )
-                })
-            }
-        </div>
-    )
+        )
+    }
 }
 
 export default Categories
